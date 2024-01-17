a9b04e29e514b741aae4763f497af69cc6254e75
JonathanWingLx <jwinguk@gmail.com>
2024-01-17 18:55:13 +0000

    Update

---
+++ Read test.py
@@ -0,0 +1,3 @@
+file = open("/Users/jonathanwing/Library/Mobile Documents/com~apple~CloudDocs/Mountview Work/Year 3/BA 302 - SRP/Git SRP Project/Git-SRP-Project/9 to 5 Paperwork Exported Data", "rt")
+
+print(file.read())
