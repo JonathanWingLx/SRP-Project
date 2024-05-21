2546bb77d2dafcf5fb951525a8b8de1e73a618e8
Jonathan <jwinguk@gmail.com>
2024-05-22 00:03:16 +0100

    Added README Added database Added To do list Added basic menu within program

--- Project/Code/Menu.py
+++ Project/Code/Menu.py
@@ -1,11 +1,38 @@
 from tkinter import *
+from tkinter.filedialog import *
 
-def Menu():
-    Menu = Tk()
-    Menu.geometry("1080x720")
+def MainScreen():
+
+    def Text():
+        print("Test")
+
+    def OpenFile():
+        filename = askopenfilename(parent=MScreen)
+        f = open(filename)
+        print(f.read())
+
+    MScreen = Tk()
+    MScreen.geometry("1080x720")
+
+    MenuBar = Menu(MScreen)
+    filemenu = Menu(MenuBar, tearoff=0)
+    filemenu.add_command(label="New", command=Text)
+    filemenu.add_command(label="Open", command=OpenFile)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=MScreen.quit)
+    MenuBar.add_cascade(label='File', menu=filemenu)
+
+    editmenu = Menu(MenuBar, tearoff=0)
+    editmenu.add_command(label="Undo", command=Text)
+
+    helpmenu = Menu(MenuBar, tearoff=0)
+
+    MScreen.config(menu=MenuBar)
+    MScreen.mainloop()
+
+MainScreen()
 
 
-
-    Menu.mainloop()
-
-Menu()
+#Documentation/Outside Sources
+#"https://www.tutorialspoint.com/python/tk_menu.htm"
+#"https://stackoverflow.com/questions/16429716/opening-file-tkinter"
